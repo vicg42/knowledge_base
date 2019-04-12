21e0ba52078459aff157292140d24ebbbfc0baa0
Viktor Golovachenko <viktor.golovachenko@axonim.by>
2019-04-12 12:36:01 +0300

    _create_firmware_ise

---
+++ xilinx/_create_firmware_ise.tcl
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------
+# Engineer    : Golovachenko Victor
+#
+# Create Date : 02.11.2018 12:23:01
+# Module Name :
+#
+# Description :
+#
+#------------------------------------------------------------------------
+
+set firmware_src ./prj/main.bit
+set firmware_dst ../fpga-firmware/fpga_firmware_juno.bit
+
+#main
+puts "firmware copy from [file normalize $firmware_src] to [file normalize $firmware_dst]"
+file copy -force $firmware_src $firmware_dst
