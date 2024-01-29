9c67be7b3613ec0c37ef31d708d0338d8bf5ce5d
v.halavachenka <v.halavachenka@cekgroup.ru>
2024-01-29 09:36:56 +0300

    add variable USER_MAC_ADDRESS

--- wsl/wsl-set-mac-for-license/wsl.mac.sh
+++ wsl/wsl-set-mac-for-license/wsl.mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+USER_MAC_ADDRESS=00:15:5d:2a:fc:a4
 # USR_MAC_ADDRES=/etc/wsl.mac.txt
 # if [[ ! -s $USR_MAC_ADDRES ]]; then
 #     #echo "save mac addres.This addres will be user mac address"
@@ -14,16 +15,14 @@ if ! ifconfig -a | grep bond0; then
 fi
 
 # #https://github.com/microsoft/WSL/issues/5352#issuecomment-1076336583
-# mac=$(cat $USR_MAC_ADDRES)
-mac=00:15:5d:2a:fc:a4
 gateway=$(ip route | awk '/default via /{print $3; exit}' 2>/dev/null)
-if ! ip link show | grep $mac; then
+if ! ip link show | grep $USER_MAC_ADDRESS; then
     sudo ip link set dev eth0 down || exit 1
     sudo ip link set dev eth0 name eth1 || exit 1
     sudo ip link set dev eth1 up || exit 1
     sudo ip route add default via $gateway dev eth1 || exit 1
     sudo ip link set dev bond0 down || exit 1
-    sudo ip link set dev bond0 address $mac || exit 1
+    sudo ip link set dev bond0 address $USER_MAC_ADDRESS || exit 1
     sudo ip link set dev bond0 name eth0 || exit 1
     sudo ip link set dev eth0 up || exit 1
 fi
